4e4bd3b56c2f304b51ac00591f4d70d791356087
Tate House <tatehouse100@gmail.com>
2023-10-07 16:00:48 -0400

    Fix: Add grid parameter to generate method
    Updates: #PMG-12

--- Core/Include/MazeGenerator.hpp
+++ Core/Include/MazeGenerator.hpp
@@ -4,6 +4,8 @@
 #include <random>
 #include <string>
 
+#include "Grid.hpp"
+
 namespace Core {
 class MazeGenerator {
 protected:
@@ -13,7 +15,7 @@ public:
 	virtual ~MazeGenerator() noexcept;
 
 public:
-	virtual void generate(const long long* seed) noexcept = 0;
+	virtual void generate(Grid* const grid, const long long* seed) noexcept = 0;
 	[[nodiscard]] const std::string& getMazeGenerationAlgorithmName() const noexcept;
 	[[nodiscard]] const std::optional<unsigned long long> getSeed() const noexcept;
 

13593e100ea95712f511e86592610427d403bdad
Tate House <tatehouse100@gmail.com>
2023-10-08 21:35:09 -0400

    Test: Mock maze generator
    Instead of relying on the BinaryTreeMazeGenerator, the MazeGenerator
    tests now rely on a mock.

--- Test/Source/MazeGeneratorTest.cpp
+++ Test/Source/MazeGeneratorTest.cpp
@@ -2,7 +2,7 @@
 
 #include <memory>
 
-#include "BinaryTreeMazeGenerator.hpp"
+#include "MockMazeGenerator.hpp"
 #include "SquareGrid.hpp"
 
 namespace Core::Test {
@@ -16,14 +16,24 @@ public:
 
 protected:
 	std::unique_ptr<Grid> grid {std::make_unique<SquareGrid<5>>()};
-	std::unique_ptr<MazeGenerator> mazeGenerator {std::make_unique<BinaryTreeMazeGenerator>()};
+	std::unique_ptr<MockMazeGenerator> mazeGenerator {std::make_unique<MockMazeGenerator>()};
 };
+
+TEST_F(MazeGeneratorTest,
+       GivenMazeGenerator_WhenGetMazeGenerationAlgorithmName_ThenReturnsMazeGenerationAlgorithmName) {
+	EXPECT_THAT(mazeGenerator->getMazeGenerationAlgorithmName(), testing::Eq("Mock Maze Generator"));
+}
 
 TEST_F(MazeGeneratorTest, GivenMazeGeneratorThatDoesNotContainSeed_WhenGetSeed_ThenReturnsNullopt) {
 	EXPECT_THAT(mazeGenerator->getSeed(), testing::Eq(std::nullopt));
 }
 
 TEST_F(MazeGeneratorTest, GivenMazeGenerator_WhenGenerateWithoutSeed_ThenUsesRandomSeed) {
+	EXPECT_CALL(*mazeGenerator, generate(grid.get(), nullptr)).Times(1).WillOnce([this](const Grid* const grid,
+	                                                                                    const unsigned long long*) {
+		mazeGenerator->getRandomEngineWrapper(nullptr);
+	});
+	
 	mazeGenerator->generate(grid.get(), nullptr);
 	
 	EXPECT_THAT(mazeGenerator->getSeed(), testing::Ne(std::nullopt));
@@ -31,6 +41,12 @@ TEST_F(MazeGeneratorTest, GivenMazeGenerator_WhenGenerateWithoutSeed_ThenUsesRan
 
 TEST_F(MazeGeneratorTest, GivenMazeGenerator_WhenGenerateWithSeed_ThenUsesSeed) {
 	const unsigned long long seed {123456789};
+	
+	EXPECT_CALL(*mazeGenerator, generate(grid.get(), &seed)).Times(1).WillOnce([this, &seed](const Grid* const grid,
+	                                                                                         const unsigned long long*) {
+		mazeGenerator->getRandomEngineWrapper(&seed);
+	});
+	
 	mazeGenerator->generate(grid.get(), &seed);
 	
 	EXPECT_THAT(mazeGenerator->getSeed(), testing::Eq(seed));

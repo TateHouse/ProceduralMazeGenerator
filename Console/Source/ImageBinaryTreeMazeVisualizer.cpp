f432d898a5284a23b2e1fad5e1a6d29ea83bf3ae
Tate House <tatehouse100@gmail.com>
2023-10-13 19:41:06 -0400

    Refactor: Visualize maze in image file
    Resolves: #PMG-4

--- Console/Source/ImageBinaryTreeMazeVisualizer.cpp
+++ Console/Source/ImageBinaryTreeMazeVisualizer.cpp
@@ -2,8 +2,8 @@
 
 namespace Console {
 ImageBinaryTreeMazeVisualizer::ImageBinaryTreeMazeVisualizer(const Core::Grid* const grid,
-                                                             const unsigned int cellSize,
-                                                             const unsigned int borderSize,
+                                                             const int cellSize,
+                                                             const int borderSize,
                                                              const std::string_view imagePath,
                                                              const cv::Scalar& backgroundColor,
                                                              const cv::Scalar& gridColor) :
@@ -13,6 +13,10 @@ ImageBinaryTreeMazeVisualizer::ImageBinaryTreeMazeVisualizer(const Core::Grid* c
 		imagePath {imagePath.data()},
 		backgroundColor {backgroundColor},
 		gridColor {gridColor} {
+	if (cellSize < 1) {
+		throw std::invalid_argument {"Cell size must be greater than 0."};
+	}
+	
 	if (borderSize < 1) {
 		throw std::invalid_argument {"Border size must be greater than 0."};
 	}
@@ -21,60 +25,93 @@ ImageBinaryTreeMazeVisualizer::ImageBinaryTreeMazeVisualizer(const Core::Grid* c
 void ImageBinaryTreeMazeVisualizer::display() noexcept {
 	const auto [gridWidth, gridHeight] {grid->getSize()};
 	
-	const auto imageWidth {static_cast<int>(cellSize * gridWidth + (2 * borderSize))};
-	const auto imageHeight {static_cast<int>(cellSize * gridHeight + (2 * borderSize))};
+	const auto imageWidth {cellSize * gridWidth + (2 * borderSize)};
+	const auto imageHeight {cellSize * gridHeight + (2 * borderSize)};
 	
 	auto image {cv::Mat {imageHeight, imageWidth, CV_8UC3, backgroundColor}};
 	
 	for (auto row {gridHeight - 1}; row >= 0; --row) {
-		for (std::size_t column {0}; column < gridWidth; ++column) {
-			const auto position {std::pair<const int, const int> {static_cast<int>(column), row}};
+		for (auto column {0}; column < gridWidth; ++column) {
+			const auto position {std::pair<const int, const int> {column, row}};
 			const auto* const cell {(*grid)[position]};
 			
-			auto topLeftY {static_cast<int>(imageHeight - (row + 1) * cellSize - borderSize)};
-			auto topLeftX {static_cast<int>(column * cellSize + borderSize)};
+			auto topLeftY {imageHeight - (row + 1) * cellSize - borderSize};
+			auto topLeftX {column * cellSize + borderSize};
 			
 			const auto* const eastNeighbor {cell->getEast()};
 			const auto* const southNeighbor {cell->getSouth()};
 			
 			if (column != gridWidth - 1 && (eastNeighbor == nullptr || !cell->isLinked(eastNeighbor))) {
-				auto adjustedTopLeftY {topLeftY};
-				if (row == 0) {
-					adjustedTopLeftY -= 1;
-				}
-				
-				const auto topPoint {cv::Point {static_cast<int>(topLeftX + cellSize), topLeftY}};
-				const auto bottomPoint {cv::Point {static_cast<int>(topLeftX + cellSize),
-				                                   static_cast<int>(adjustedTopLeftY + cellSize)}};
+				const auto [topPoint, bottomPoint] {calculateVerticalPoints(row, topLeftX, topLeftY)};
 				cv::line(image, topPoint, bottomPoint, gridColor);
 			}
 			
 			if (row != 0 && (southNeighbor == nullptr || !cell->isLinked(southNeighbor))) {
-				const auto leftPoint {cv::Point {topLeftX, static_cast<int>(topLeftY + cellSize)}};
-				const auto rightPoint {cv::Point {static_cast<int>(topLeftX + cellSize),
-				                                  static_cast<int>(topLeftY + cellSize)}};
+				const auto [leftPoint, rightPoint] {calculateHorizontalPoints(topLeftX, topLeftY)};
 				cv::line(image, leftPoint, rightPoint, gridColor);
 			}
 		}
 	}
 	
-	cv::line(image,
-	         cv::Point(borderSize, borderSize),
-	         cv::Point(static_cast<int>(imageWidth - borderSize - 1), borderSize),
-	         gridColor);
-	cv::line(image,
-	         cv::Point(borderSize, borderSize),
-	         cv::Point(borderSize, static_cast<int>(imageHeight - (borderSize + 1))),
-	         gridColor);
-	cv::line(image,
-	         cv::Point(borderSize, imageHeight - borderSize - 1),
-	         cv::Point(imageWidth - borderSize - 1, imageHeight - borderSize - 1),
-	         gridColor);
-	cv::line(image,
-	         cv::Point(imageWidth - borderSize - 1, borderSize),
-	         cv::Point(imageWidth - borderSize - 1, imageHeight - borderSize - 1),
-	         gridColor);
+	drawOuterNorthWall(imageWidth, image);
+	drawOuterWestWall(imageHeight, image);
+	drawOuterSouthWall(imageWidth, imageHeight, image);
+	drawOuterEastWall(imageWidth, imageHeight, image);
 	
 	cv::imwrite(imagePath, image);
+}
+
+std::pair<const cv::Point, const cv::Point> ImageBinaryTreeMazeVisualizer::calculateVerticalPoints(const int row,
+                                                                                                   const int topLeftX,
+                                                                                                   const int topLeftY) const noexcept {
+	auto adjustedTopLeftY {topLeftY};
+	if (row == 0) {
+		adjustedTopLeftY -= 1;
+	}
+	
+	const auto topPoint {cv::Point(topLeftX + cellSize, topLeftY)};
+	const auto bottomPoint {cv::Point(topLeftX + cellSize, adjustedTopLeftY + cellSize)};
+	
+	return {topPoint, bottomPoint};
+}
+
+std::pair<const cv::Point, const cv::Point> ImageBinaryTreeMazeVisualizer::calculateHorizontalPoints(const int topLeftX,
+                                                                                                     const int topLeftY) const noexcept {
+	const auto leftPoint {cv::Point(topLeftX, topLeftY + cellSize)};
+	const auto rightPoint {cv::Point(topLeftX + cellSize, topLeftY + cellSize)};
+	
+	return {leftPoint, rightPoint};
+}
+
+void ImageBinaryTreeMazeVisualizer::drawOuterNorthWall(int imageWidth, const cv::Mat& image) const noexcept {
+	const auto leftPoint {cv::Point(borderSize, borderSize)};
+	const auto rightPoint {cv::Point(imageWidth - borderSize - 1, borderSize)};
+	
+	cv::line(image, leftPoint, rightPoint, gridColor);
+}
+
+void ImageBinaryTreeMazeVisualizer::drawOuterWestWall(int imageHeight, const cv::Mat& image) const noexcept {
+	const auto topPoint {cv::Point(borderSize, borderSize)};
+	const auto bottomPoint {cv::Point(borderSize, imageHeight - borderSize - 1)};
+	
+	cv::line(image, topPoint, bottomPoint, gridColor);
+}
+
+void ImageBinaryTreeMazeVisualizer::drawOuterSouthWall(int imageWidth,
+                                                       int imageHeight,
+                                                       const cv::Mat& image) const noexcept {
+	const auto leftPoint {cv::Point(borderSize, imageHeight - borderSize - 1)};
+	const auto rightPoint {cv::Point(imageWidth - borderSize - 1, imageHeight - borderSize - 1)};
+	
+	cv::line(image, leftPoint, rightPoint, gridColor);
+}
+
+void ImageBinaryTreeMazeVisualizer::drawOuterEastWall(int imageWidth,
+                                                      int imageHeight,
+                                                      const cv::Mat& image) const noexcept {
+	const auto topPoint {cv::Point(imageWidth - borderSize - 1, borderSize)};
+	const auto bottomPoint {cv::Point(imageWidth - borderSize - 1, imageHeight - borderSize - 1)};
+	
+	cv::line(image, topPoint, bottomPoint, gridColor);
 }
 }
--- Console/Source/Main.cpp
+++ Console/Source/Main.cpp
@@ -8,12 +8,11 @@
 #include "SquareGrid.hpp"
 
 int main(int argc, char* argv[]) {
-	static constexpr std::size_t MAZE_SIZE {10};
+	static constexpr std::size_t MAZE_SIZE {25};
 	std::unique_ptr<Core::Grid> maze {std::make_unique<Core::SquareGrid<MAZE_SIZE>>()};
 	maze->initialize();
 	
-	std::unique_ptr<Core::GridVisualizer> imageGrid {std::make_unique<Console::ImageGridVisualizer>(maze.get(), 50,
-	                                                                                                1,
+	std::unique_ptr<Core::GridVisualizer> imageGrid {std::make_unique<Console::ImageGridVisualizer>(maze.get(), 50, 10,
 	                                                                                                "Images/Grid.png",
 	                                                                                                cv::Scalar(255,
 	                                                                                                           255,
@@ -31,16 +30,16 @@ int main(int argc, char* argv[]) {
 	std::cout << '\n';
 	
 	std::unique_ptr<Core::GridVisualizer> imageMaze {std::make_unique<Console::ImageBinaryTreeMazeVisualizer>(maze.get(),
-	                                                                                                          20,
-	                                                                                                          1,
+	                                                                                                          50,
+	                                                                                                          5,
 	                                                                                                          "Images/Maze.png",
 	                                                                                                          cv::Scalar(
 			                                                                                                          255,
 			                                                                                                          255,
 			                                                                                                          255),
 	                                                                                                          cv::Scalar(
-			                                                                                                          110,
-			                                                                                                          20,
+			                                                                                                          0,
+			                                                                                                          0,
 			                                                                                                          0))};
 	imageMaze->display();
 	

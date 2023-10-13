31b4959edaa27dc2cabc6af1ed19f516121b1c4b
Tate House <tatehouse100@gmail.com>
2023-10-13 19:56:06 -0400

    Refactor: Visualize grid in image file

--- Console/Include/ImageGridVisualizer.hpp
+++ Console/Include/ImageGridVisualizer.hpp
@@ -10,9 +10,7 @@
 namespace Console {
 class ImageGridVisualizer : public Core::GridVisualizer {
 public:
-	explicit ImageGridVisualizer(const Core::Grid* const grid,
-	                             const unsigned int cellSize,
-	                             const unsigned int borderSize,
+	explicit ImageGridVisualizer(const Core::Grid* const grid, const int cellSize, const int borderSize,
 	                             const std::string_view imagePath,
 	                             const cv::Scalar& backgroundColor,
 	                             const cv::Scalar& gridColor);
@@ -20,9 +18,20 @@ public:
 	virtual void display() noexcept override;
 
 private:
+	std::pair<const cv::Point, const cv::Point> calculateVerticalPoints(const int column,
+	                                                                    const int imageHeight) const noexcept;
+	std::pair<const cv::Point, const cv::Point> calculateHorizontalPoints(const int row,
+	                                                                      const int imageWidth) const noexcept;
+	
+	void drawOuterNorthWall(const int imageWidth, cv::Mat& image) const noexcept;
+	void drawOuterWestWall(const int imageHeight, cv::Mat& image) const noexcept;
+	void drawOuterSouthWall(const int imageWidth, const int imageHeight, cv::Mat& image) const noexcept;
+	void drawOuterEastWall(const int imageWidth, const int imageHeight, cv::Mat& image) const noexcept;
+
+private:
 	const Core::Grid* const grid;
-	const unsigned int cellSize;
-	const unsigned int borderSize;
+	const int cellSize;
+	const int borderSize;
 	const std::string imagePath;
 	const cv::Scalar backgroundColor;
 	const cv::Scalar gridColor;

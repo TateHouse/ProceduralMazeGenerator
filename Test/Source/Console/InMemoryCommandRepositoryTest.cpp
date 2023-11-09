c0428ee10db9fc431d49055ba59cad6b399163e8
Tate House <tatehouse100@gmail.com>
2023-11-08 23:54:32 -0500

    Test: Grid command
    The grid command tests were moved from the InMemoryCommandRepositoryTest
    into their own class.

--- Test/Source/Console/InMemoryCommandRepositoryTest.cpp
+++ Test/Source/Console/InMemoryCommandRepositoryTest.cpp
@@ -25,43 +25,4 @@ TEST_F(InMemoryCommandRepositoryTest,
 
     EXPECT_THAT(context.getIsRunning(), testing::IsFalse());
 }
-
-TEST_F(InMemoryCommandRepositoryTest,
-       GivenInMemoryCommandRepository_WhenExecuteGridCommandWithoutParameter_ThenGridIsGeneratedWithDefaultSize) {
-    const std::string name {"grid"};
-    const auto parameters {std::unordered_map<std::string, std::string> {}};
-
-    commandRepository.execute(name, parameters);
-
-    const auto [width, height] {context.getGrid()->getSize()};
-
-    EXPECT_THAT(width, testing::Eq(10));
-    EXPECT_THAT(height, testing::Eq(10));
-}
-
-TEST_F(InMemoryCommandRepositoryTest,
-       GivenInMemoryCommandRepository_WhenExecuteGridCommandWithParameterButWithoutParameterValue_ThenGridIsGeneratedWithDefaultSize) {
-    const std::string name {"grid"};
-    const auto parameters {std::unordered_map<std::string, std::string> {{"-s", ""}}};
-
-    commandRepository.execute(name, parameters);
-
-    const auto [width, height] {context.getGrid()->getSize()};
-
-    EXPECT_THAT(width, testing::Eq(10));
-    EXPECT_THAT(height, testing::Eq(10));
-}
-
-TEST_F(InMemoryCommandRepositoryTest,
-       GivenInMemoryCommandRepository_WhenExecuteGridCommandWithParameterAndParameterValue_ThenGridIsGeneratedWithParameterValue) {
-    const std::string name {"grid"};
-    const auto parameters {std::unordered_map<std::string, std::string> {{"-s", "20"}}};
-
-    commandRepository.execute(name, parameters);
-
-    const auto [width, height] {context.getGrid()->getSize()};
-
-    EXPECT_THAT(width, testing::Eq(20));
-    EXPECT_THAT(height, testing::Eq(20));
-}
 }

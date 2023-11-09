6487488c6979de53594755dab95ea82399d5e2ed
Tate House <tatehouse100@gmail.com>
2023-11-09 12:46:57 -0500

    Feature: Get help for specific command

--- Console/Source/Commands/HelpCommand.cpp
+++ Console/Source/Commands/HelpCommand.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <sstream>
 
+#include "Commands/CommandParametersUtility.hpp"
+
 namespace Console {
 HelpCommand::HelpCommand(Console::Context& context) noexcept:
         Command {context} {
@@ -10,11 +12,28 @@ HelpCommand::HelpCommand(Console::Context& context) noexcept:
 }
 
 void HelpCommand::execute(const std::unordered_map<std::string, std::string>& parameters) {
-    std::cout << "====================================================" << '\n';
-    for (const auto& [name, description] : commands) {
-        std::cout << name << ':' << '\n';
-        std::cout << description << '\n';
+    if (parameters.empty()) {
+        std::cout << "====================================================" << '\n';
+
+        for (const auto& [name, description] : commands) {
+            std::cout << name << ':' << '\n';
+            std::cout << description << '\n';
+        }
+
+        std::cout << "====================================================" << '\n';
+        return;
     }
+
+    const auto commandName {CommandParametersUtility::getParameterValue(parameters, "-c")};
+    const auto iterator {commands.find(commandName)};
+    if (iterator == commands.end()) {
+        std::cout << "The specified command does not exist" << '\n';
+        return;
+    }
+
+    std::cout << "====================================================" << '\n';
+    std::cout << iterator->first << ':' << '\n';
+    std::cout << iterator->second << '\n';
     std::cout << "====================================================" << '\n';
 }
 
@@ -27,8 +46,19 @@ std::string HelpCommand::getDescription() const noexcept {
 
     descriptionStream << "\tDisplays the name and description of all commands.\n\n";
 
+    descriptionStream << "\tParameters (Optional):\n"
+                      << "\t\t-c <command name>\n"
+                      << "\t\t\tDisplays the name and description of the specified command.\n\n";
+
     descriptionStream << "\tUsage:\n"
-                      << "\t\t" << getName() << '\n';
+                      << "\t\thelp\n"
+                      << "\t\thelp -c <command name>\n\n";
+
+    descriptionStream << "\tExamples:\n"
+                      << "\t\thelp\n"
+                      << "\t\tThis will display the name and description of all commands.\n\n"
+                      << "\t\thelp -c maze\n\n"
+                      << "\t\tThis will display the name and description of the maze command.";
 
     return descriptionStream.str();
 }

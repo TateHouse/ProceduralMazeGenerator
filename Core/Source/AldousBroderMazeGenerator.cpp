d78c63f0a55a0adf47d6258eb40be6dda5666828
Tate House <tatehouse100@gmail.com>
2023-11-20 16:02:44 -0500

    Refactor: Aldous-Border

--- Core/Source/AldousBroderMazeGenerator.cpp
+++ Core/Source/AldousBroderMazeGenerator.cpp
@@ -7,44 +7,44 @@ AldousBroderMazeGenerator::AldousBroderMazeGenerator() noexcept: MazeGenerator {
 
 void AldousBroderMazeGenerator::generate(Core::Grid* const grid,
                                          const std::optional<unsigned long long>& seed) noexcept {
-    auto randomEngine {getRandomEngine(seed)};
-    std::uniform_int_distribution<std::size_t> distribution {};
-    const auto [width, height] {grid->getSize()};
-    const auto randomX {static_cast<int>(distribution(randomEngine) % width)};
-    const auto randomY {static_cast<int>(distribution(randomEngine) % height)};
-    auto* cell {(*grid)[{randomX, randomY}]};
-    const auto totalCells {width * height};
-    auto unvisitedCells {totalCells - 1};
-
-    while (unvisitedCells > 0) {
-        std::vector<Cell*> neighbors {};
-
-        if (cell->getNorth() != nullptr) {
-            neighbors.push_back(cell->getNorth());
-        }
-
-        if (cell->getWest() != nullptr) {
-            neighbors.push_back(cell->getWest());
-        }
-
-        if (cell->getSouth() != nullptr) {
-            neighbors.push_back(cell->getSouth());
-        }
-
-        if (cell->getEast() != nullptr) {
-            neighbors.push_back(cell->getEast());
-        }
-
-        distribution.param(std::uniform_int_distribution<std::size_t>::param_type {0, neighbors.size() - 1});
-        const auto index {distribution(randomEngine) % neighbors.size()};
-        auto* neighbor {neighbors[index]};
-
-        if (neighbor->getLinks().empty()) {
-            cell->link(neighbor);
-            --unvisitedCells;
-        }
-
-        cell = neighbor;
-    }
+	auto randomEngine {getRandomEngine(seed)};
+	std::uniform_int_distribution<std::size_t> distribution {};
+	const auto [width, height] {grid->getSize()};
+	const auto randomX {static_cast<int>(distribution(randomEngine) % width)};
+	const auto randomY {static_cast<int>(distribution(randomEngine) % height)};
+	auto* cell {(*grid)[{randomX, randomY}]};
+	const auto totalCells {width * height};
+	auto unvisitedCells {totalCells - 1};
+	
+	while (unvisitedCells > 0) {
+		std::vector<Cell*> neighbors {};
+		
+		if (auto* northCell {cell->getNorth()}; northCell != nullptr) {
+			neighbors.push_back(northCell);
+		}
+		
+		if (auto* westCell {cell->getWest()}; westCell != nullptr) {
+			neighbors.push_back(westCell);
+		}
+		
+		if (auto* southCell {cell->getSouth()}; southCell != nullptr) {
+			neighbors.push_back(southCell);
+		}
+		
+		if (auto* eastCell {cell->getEast()}; eastCell != nullptr) {
+			neighbors.push_back(eastCell);
+		}
+		
+		distribution.param(std::uniform_int_distribution<std::size_t>::param_type {0, neighbors.size() - 1});
+		const auto index {distribution(randomEngine) % neighbors.size()};
+		auto* neighbor {neighbors[index]};
+		
+		if (neighbor->getLinks().empty()) {
+			cell->link(neighbor);
+			--unvisitedCells;
+		}
+		
+		cell = neighbor;
+	}
 }
 }

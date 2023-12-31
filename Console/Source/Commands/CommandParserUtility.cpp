c9167b1a109c5d53454c41dc7d6b4c1d8f24b9c7
Tate House <tatehouse100@gmail.com>
2023-12-30 23:27:33 -0500

    Refactor: Remove redundant method call

--- Console/Source/Commands/CommandParserUtility.cpp
+++ Console/Source/Commands/CommandParserUtility.cpp
@@ -5,7 +5,7 @@
 namespace Console {
 std::string CommandParametersUtility::getParameterValue(const std::unordered_map<std::string, std::string>& parameters,
                                                         const std::string& parameterName) {
-    auto iterator {parameters.find(parameterName.data())};
+    auto iterator {parameters.find(parameterName)};
     if (iterator == parameters.end()) {
         throw std::invalid_argument {"Parameter not found: " + parameterName};
     }
